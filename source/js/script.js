0a327111bf30c06b6ff2167f36bdf21376ba13ef
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2019-08-18 22:21:33 +0300

    Add yandex map

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,57 @@ menuBtn.addEventListener('click', function() {
         mainNav.classList.remove('main-nav--opened');
     }
 });
+
+var modalBlock = document.querySelector("#modal");
+var modalOverlay = document.querySelector("#modal__overlay");
+var modalOpenButton = document.querySelector(".modal__open-btn");
+
+var modal = function(modalObj, modalOverlay) {
+  modalObj.classList.toggle("modal--close");
+  modalOverlay.classList.toggle("modal--close");
+};
+
+modalOpenButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal(modalBlock, modalOverlay);
+});
+
+modalOverlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal(modalBlock, modalOverlay);
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!modalBlock.classList.contains("modal-close")) {
+      modal(modalBlock, modalOverlay);
+    }
+  }
+});
+
+ymaps.ready(function (){
+
+  var myMap;
+
+  myMap = new ymaps.Map("map", {
+    center: [59.938631, 30.323055],
+    zoom: 17,
+    controls: []
+  });
+
+  myMap.behaviors.disable('scrollZoom');
+
+  myMap.controls.add("zoomControl", {
+    position: {top: 15, left: 15}
+  });
+
+  var myPlacemark = new ymaps.Placemark([59.938631, 30.323055] , {},
+    { iconLayout: 'default#image',
+      iconImageHref: 'img/icon-map-pin.svg',
+      iconImageSize: [66, 101],
+      iconImageOffset: [-35, -96] });
+
+  myMap.geoObjects.add(myPlacemark);
+
+});
